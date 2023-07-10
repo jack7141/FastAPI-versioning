319031bf02ac5844fca38275c3d24e721cf89693
황광회 <jack7141@nate.com>
2023-07-10 22:16:58 +0900

    db init

---
+++ migrations/versions/38bddea6a315_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 38bddea6a315
+Revises: 
+Create Date: 2023-07-10 21:53:47.640314
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38bddea6a315'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
